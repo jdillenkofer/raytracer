665b25e0c9eba242a54d1c96a5bdbd2972acb4d4
Jan Dillenkofer <jan.dillenkofer@t-online.de>
2018-12-06 07:46:42 +0100

    Refactor intersection code

--- src/raytracer.c
+++ src/raytracer.c
@@ -1,37 +1,31 @@
 #include "raytracer.h"
 
 #include <float.h>
+#include <stdbool.h>
 
-static void raytracer_intersectPlanes(Scene *scene, Ray *ray, double *hitDistance, uint32_t* hitMaterialIndex) {
-    for (uint32_t i = 0; i < scene->planeCount; i++) {
-        Plane plane = scene->planes[i];
-
+static bool raytracer_intersectPlane(Plane *plane, Ray *ray, double *hitDistance) {
         // We use the "Hesse normal form":
         //     normal * p - distanceFromOrigin = 0
         // to describe our planes
-        double denominator = vec3_dot(plane.normal, ray->direction);
+        double denominator = vec3_dot(plane->normal, ray->direction);
         if ((denominator < -EPSILON) || (denominator > EPSILON)) {
-            double t = (-plane.distanceFromOrigin - vec3_dot(plane.normal, ray->origin)) / denominator;
-            // only hit objects in front of us,
-            // and if there are closer than the currently closest hit
-            if ((t > 0) && (t < *hitDistance)) {
+            double t = (-plane->distanceFromOrigin - vec3_dot(plane->normal, ray->origin)) / denominator;
+            // only hit objects in front of us
+            if (t > 0) {
                 *hitDistance = t;
-                *hitMaterialIndex = plane.materialIndex;
+                return true;
             }
         }
-    }
+        return false;
 }
 
-static void raytracer_intersectSpheres(Scene *scene, Ray *ray, double *hitDistance, uint32_t* hitMaterialIndex) {
-    for (uint32_t i = 0; i < scene->sphereCount; i++) {
-        Sphere sphere = scene->spheres[i];
-
-        Vec3 sphereRelativeOrigin = vec3_sub(ray->origin, sphere.position);
+static bool raytracer_intersectSphere(Sphere *sphere, Ray *ray, double *hitDistance) {
+        Vec3 sphereRelativeOrigin = vec3_sub(ray->origin, sphere->position);
 
         // Mitternachtsformel
         double a = vec3_dot(ray->direction, ray->direction);
         double b = 2.0f * vec3_dot(ray->direction, sphereRelativeOrigin);
-        double c = vec3_dot(sphereRelativeOrigin, sphereRelativeOrigin) - sphere.radius * sphere.radius;
+        double c = vec3_dot(sphereRelativeOrigin, sphereRelativeOrigin) - sphere->radius * sphere->radius;
 
         double denominator = 2.0f * a;
         double squareRootTerm = sqrt(b*b - 4.0f * a * c);
@@ -42,33 +36,49 @@ static void raytracer_intersectSpheres(Scene *scene, Ray *ray, double *hitDistan
 
             double t = tpos;
             // only hit objects in front of us,
-            // and if there are closer than the currently closest hit
             if ((tneg > 0) && (tneg < tpos)) {
                 t = tneg;
             }
-            if ((t > 0) && (t < *hitDistance)) {
+            if (t > 0) {
                 *hitDistance = t;
-                *hitMaterialIndex = sphere.materialIndex;
+                return true;
             }
         }
-    }
 }
 
 Vec3 raytracer_raycast(Scene* scene, Ray* ray) {
     Vec3 outColor = (Vec3) { 0.0f, 0.0f, 0.0f };
 
-    double hitDistance = DBL_MAX;
+    double minHitDistance = DBL_MAX;
     uint32_t hitMaterialIndex = 0;
 
-    raytracer_intersectPlanes(scene, ray, &hitDistance, &hitMaterialIndex);
-    raytracer_intersectSpheres(scene, ray, &hitDistance, &hitMaterialIndex);
+    for (uint32_t i = 0; i < scene->planeCount; i++) {
+        Plane* plane = &scene->planes[i];
+        double planeHitDistance = DBL_MAX;
+        if (raytracer_intersectPlane(plane, ray, &planeHitDistance)) {
+            if (planeHitDistance < minHitDistance) {
+                minHitDistance = planeHitDistance;
+                hitMaterialIndex = plane->materialIndex;
+            }
+        }
+    }
+    for (uint32_t i = 0; i < scene->sphereCount; i++) {
+        Sphere* sphere = &scene->spheres[i];
+        double sphereHitDistance = DBL_MAX;
+        if (raytracer_intersectSphere(sphere, ray, &sphereHitDistance)) {
+            if (sphereHitDistance < minHitDistance) {
+                minHitDistance = sphereHitDistance;
+                hitMaterialIndex = sphere->materialIndex;
+            }
+        }
+    }
 
     // if we got a hit calculate the hitPoint and send a shadow rays to each lightsource
     if (hitMaterialIndex) {
 
         Material hitMaterial = scene->materials[hitMaterialIndex];
 
-        Vec3 hitPoint = vec3_add(ray->origin, vec3_mul(ray->direction, hitDistance));
+        Vec3 hitPoint = vec3_add(ray->origin, vec3_mul(ray->direction, minHitDistance));
         for (uint32_t i = 0; i < scene->pointLightCount; i++) {
             PointLight pointLight = scene->pointLights[i];
             Ray shadowRay = {0};
@@ -78,11 +88,29 @@ Vec3 raytracer_raycast(Scene* scene, Ray* ray) {
             // move the ray origin slightly forward to prevent self intersection
             // "surface agne"
             shadowRay.origin = vec3_add(hitPoint, vec3_mul(shadowRay.direction,1.0f/1000.0f));
-            double shadowRayHitDistance = DBL_MAX;
-            uint32_t shadowRayMaterialIndex;
-            raytracer_intersectPlanes(scene, &shadowRay, &shadowRayHitDistance, &shadowRayMaterialIndex);
-            raytracer_intersectSpheres(scene, &shadowRay, &shadowRayHitDistance, &shadowRayMaterialIndex);
-            if (distanceToLight < shadowRayHitDistance) {
+
+            double closestHitDistance = DBL_MAX;
+            for (uint32_t x = 0; x < scene->planeCount; x++) {
+                Plane *plane = &scene->planes[x];
+                double shadowRayPlaneHitDistance = DBL_MAX;
+
+                if (raytracer_intersectPlane(plane, &shadowRay, &shadowRayPlaneHitDistance)) {
+                    if (shadowRayPlaneHitDistance < closestHitDistance) {
+                        closestHitDistance = shadowRayPlaneHitDistance;
+                    }
+                }
+            }
+            for (uint32_t x = 0; x < scene->sphereCount; x++) {
+                Sphere *sphere = &scene->spheres[x];
+                double shadowRaySphereDistance = DBL_MAX;
+
+                if (raytracer_intersectSphere(sphere, &shadowRay, &shadowRaySphereDistance)) {
+                    if (shadowRaySphereDistance < closestHitDistance) {
+                        closestHitDistance = shadowRaySphereDistance;
+                    }
+                }
+            }
+            if (distanceToLight < closestHitDistance) {
                 // we hit the light
                 outColor = vec3_add(outColor, pointLight.emissionColor);
             }
